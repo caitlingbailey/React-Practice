cffa13b3837de5759b9a5090fec33f6186d34f07
Caitlin <caitlin.revmo.app>
2021-06-22 14:41:57 +0100

    Added lifecycle methods to Counter

--- lifecycle-methods/src/components/Counter.js
+++ lifecycle-methods/src/components/Counter.js
@@ -5,21 +5,45 @@ export default class Counter extends React.Component {
         super(props)
         console.log('Constructor')
         this.state = {
-            counter: 0
+            counter: 0,
         }
         this.increment = () => this.setState({counter: this.state.counter + 1})
-        this.decrement = () => this.setState({counter: this.state.counter + 1})
+        this.decrement = () => this.setState({counter: this.state.counter - 1})
+    }
 
+    componentDidMount() {
+        console.log('Component Did Mount')
+        console.log('-----------------------')
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        if(nextProps.ignoreProp &&
+            this.state.ignoreProp !== nextProps.ignoreProp) {
+                return false
+            }
+        return true
     }
 
     render() {
+        console.log('Render')
+
         return (
             <div>
-                but
+                <button onClick={this.increment}>Increment</button>
+                <button onClick={this.decrement}>Decrement</button>
                 <div className="counter">
-                    Counter: {0}
+                    Counter: {this.state.counter}
                 </div>
             </div>
         )
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        console.log("Component Did Update")
+    }
+
+    componentWillUnmount() {
+        console.log("Component Will Unmount")
+        console.log("----------------------")
     }
 }
