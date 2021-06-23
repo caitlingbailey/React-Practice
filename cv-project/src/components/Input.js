8f2695c58d03ae203eff4ede35a40766a4e3c2de
Caitlin <caitlin.revmo.app>
2021-06-23 15:00:12 +0100

    Setup CV builder

---
+++ cv-project/src/components/Input.js
@@ -0,0 +1,16 @@
+function Input() {
+    return (
+      <div className="input">
+          <div className="information-input">
+              <div id="personal-information">
+                  <form>
+                      <input type="text">First Name</input>
+                  </form>
+              </div>
+          </div>
+      </div>
+    );
+  }
+  
+  export default Input;
+  
